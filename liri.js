8a2e22a2ce1f49fc840272475a3872906acff974
Yolisa Bam <yolisabam@Yolisas-MacBook-Pro.local>
2017-10-14 18:38:08 -0700

    putting twitter call into a function

--- liri.js
+++ liri.js
@@ -1,21 +1,35 @@
 
 // Link in keys file
 var myKeys = require("./keys.js");
-// Link in twitter node package
-var twitter = require("twitter");
-// Link in twitter keys
-var client = new twitter(myKeys.twitterKeys);
-// console.log(myKeys.twitterKeys);
+// Create a variable for my commands
+var liriCommand = process.argv[2]
 
-var params = {screen_name: "yolisabam"};
+getTweets();
 
-client.get("statuses/user_timeline", params, function(error, tweets, response){
-  if(!error) {
-    for (var i=0; i<21; i++){
-      console.log(tweets[i].text);
-      console.log(tweets[i].created_at);      
-      console.log('******************************************');
-    }
-  } 
-})
+
+
+
+
+
+
+
+
+function getTweets(){
+  // Link in twitter node package
+  var Twitter = require("twitter");
+  var client = new Twitter(myKeys.twitterKeys);
+  // console.log(myKeys.twitterKeys);
+  var params = {screen_name: "kimkardashian"};
+  if (liriCommand === "my-tweets"){
+    client.get("statuses/user_timeline", params, function(error, tweets, response){
+      if(!error) {
+        for (var i=0; i<20; i++){
+          console.log(tweets[i].text);
+          console.log(tweets[i].created_at);      
+          console.log('******************************************');
+        }
+      } 
+    })
+  }
+}   
 
