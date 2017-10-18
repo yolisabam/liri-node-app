fe06eda9f226ddf5fc4cc3d4a715f90cbcb88e1a
Yolisa Bam <yolisabam@Yolisas-MacBook-Pro.local>
2017-10-17 22:58:02 -0700

    finishing OMDB function

--- liri.js
+++ liri.js
@@ -28,14 +28,23 @@ userCommands(liriCommand, userInput);
 function getMovies() {
 
   // Then run a request to the OMDB API with the movie specified
-  request("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=40e9cece", function(error, response, body) {
+  request("http://www.omdbapi.com/?t=" + userInput + "&y=&plot=short&apikey=40e9cece", function(error, response, body) {
 
     // If the request is successful (i.e. if the response status code is 200)
     if (!error && response.statusCode === 200) {
 
       // Parse the body of the site and recover just the imdbRating
       // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
-      console.log("The movie's rating is: " + JSON.parse(body).imdbRating);
+      console.log("********************************");
+      console.log("Movie title: " + JSON.parse(body).Title);
+      console.log("Release Year: " + JSON.parse(body).Year);
+      console.log("IMDB rating: " + JSON.parse(body).imdbRating);
+      console.log("Rotten tomato rating: " + JSON.parse(body).Ratings[1].Value);
+      console.log("Production language: " + JSON.parse(body).Country);
+      console.log("Language: " + JSON.parse(body).Language);
+      console.log("Plot: " + JSON.parse(body).Plot);
+      console.log("Actors: " + JSON.parse(body).Actors);
+      console.log("********************************");
     }
   });
 
@@ -59,13 +68,6 @@ function doWhatItSays(){
   });
 }
   
-
-
-
-
-
-
-
 
 function getTweets(){
   // Link in twitter node package
@@ -118,6 +120,7 @@ function userCommands(liriCommand, input) {
     getSongs();
   } else if(liriCommand === "do-what-it-says"){
     doWhatItSays();
+  } else if(liriCommand === "movie-this") {
+    getMovies();
   }
-
 } 
